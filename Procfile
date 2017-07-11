15dcd834186826603e33b93cd5cefa61b81db6d8
niyas <niyasahamedpkd@gmil.com>
2017-07-11 15:24:10 +0530

    allowed hosts

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn contacts.wsgi --log-file -
+worker: python worker.py
--- contacts/settings.py
+++ contacts/settings.py
@@ -24,8 +24,6 @@ SECRET_KEY = 'vg)03%44&e-mtl+f)v=wrxayvbkgj18y$7m7b6qv@k#zv-b=^q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
